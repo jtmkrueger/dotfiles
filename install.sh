55105754aeadb8f78d9fb4ebc20e7c368821f738
Krueger, John <john.krueger@nrel.gov>
2024-04-03 14:41:43 -0400

    typo

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ git checkout stable
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 make install
 
-echon "Install vim-plug and plugins"
+echo "Install vim-plug and plugins"
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
