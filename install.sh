e1ae9bc34eed410ee1deb241a80af2815cecd59d
Krueger, John <john.krueger@nrel.gov>
2024-04-03 14:18:03 -0400

    back to apt on bookworm

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
 ln -s ~/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
 
 # installing neovim & dependencies via apt
-# apt update -y && apt install -y lua5.3 python3-pip
+apt update -y && apt install -y lua5.3 python3-pip neovim
 
-curl -LO https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
-apt install ./nvim-linux64.deb
+# curl -LO https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
+# apt install ./nvim-linux64.deb
 
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
