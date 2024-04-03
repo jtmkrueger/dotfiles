7ff1b73a7a84dfb28072eaa1c8c4b94d01b75c81
Krueger, John <john.krueger@nrel.gov>
2024-04-03 12:17:53 -0400

    cleanup

--- install.sh
+++ install.sh
@@ -8,8 +8,6 @@ ln -s init.vim ~/.config/nvim/init.vim
 ln -s neo.vim ~/.config/nvim/neo.vim
 ln -s coc-settings.json ~/.config/nvim/coc-settings.json
 
-
-
 # This will bootstrap neovim by
 # * installing neovim via apt
 # * installing vim-plug
