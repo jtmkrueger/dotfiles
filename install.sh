677aa13f39299a0d7171d9611dbb8476a042246e
Krueger, John <john.krueger@nrel.gov>
2024-04-03 14:31:22 -0400

    nah we gotta build from source

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# this script can be triggered to run via devcontainer up:
+# devcontainer up --dotfiles-repository https://github.com/jtmkrueger/dotfiles --remove-existing-container
 
 set -e
 
@@ -8,7 +11,13 @@ ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
 ln -s ~/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
 
 # installing neovim & dependencies via apt
-apt update -y && apt install -y lua5.3 python3-pip neovim
+apt update -y && apt install -y lua5.3 python3-pip ninja-build gettext cmake unzip curl build-essential
+
+git clone --depth 1 https://github.com/neovim/neovim.git
+cd neovim
+git checkout stable
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+make install
 
 # curl -LO https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
 # apt install ./nvim-linux64.deb
