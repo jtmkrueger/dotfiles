813e615096f69f41c5f182cd28bcae20234398ae
Krueger, John <john.krueger@nrel.gov>
2024-04-03 13:12:18 -0400

    dont update first

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
 ln -s ~/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
 
 # installing neovim dependencies via apt
- apt update -y && apt install -y ninja-build gettext cmake unzip curl build-essential lua5.3 python3-pip
+apt install -y ninja-build gettext cmake unzip curl build-essential lua5.3 python3-pip
 
 git clone https://github.com/neovim/neovim.git
 cd neovim
