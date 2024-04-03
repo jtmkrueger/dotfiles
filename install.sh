b3f4d867036018fa26114f39ac613987a2a8af51
Krueger, John <john.krueger@nrel.gov>
2024-04-03 12:50:37 -0400

    install lua and python

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
 ln -s ~/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
 
 # installing neovim & dependencies via apt
-apt update -y && apt install -y neovim libstdc++6
+apt update -y && apt install -y lua5.3 neovim python3-pip
 
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
