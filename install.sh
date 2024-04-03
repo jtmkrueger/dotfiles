14c5ecda207ed8c15483d8174a4902d95c09c927
Krueger, John <john.krueger@nrel.gov>
2024-04-03 13:27:31 -0400

    think im stuck on the deb package

--- install.sh
+++ install.sh
@@ -7,13 +7,8 @@ mkdir -p ~/.config/nvim
 ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
 ln -s ~/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
 
-# installing neovim dependencies via apt
-apt update -y && apt install -y ninja-build gettext cmake unzip curl build-essential lua5.3 python3-pip
-
-git clone https://github.com/neovim/neovim.git
-cd neovim
-make CMAKE_BUILD_TYPE=Release
-make install
+# installing neovim & dependencies via apt
+apt update -y && apt install -y lua5.3 neovim python3-pip
 
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
