a3ecf7d86459f4b3b832672dcaa66201721922ad
Krueger, John <john.krueger@nrel.gov>
2024-04-03 12:25:37 -0400

    need paths?

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ set -e
 
 # symlink the neovim config
 mkdir -p ~/.config/nvim
-ln -s init.vim ~/.config/nvim/init.vim
-ln -s neo.vim ~/.config/nvim/neo.vim
-ln -s coc-settings.json ~/.config/nvim/coc-settings.json
+ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles/neo.vim ~/.config/nvim/neo.vim
+ln -s ~/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
 
 # installing neovim via apt
 apt update -y && apt install -y neovim
