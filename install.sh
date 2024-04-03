ccbeae83d7b859a7a0728bbb1661678fcaf90550
Krueger, John <john.krueger@nrel.gov>
2024-04-03 13:01:01 -0400

    try appimage

--- install.sh
+++ install.sh
@@ -7,8 +7,12 @@ mkdir -p ~/.config/nvim
 ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
 ln -s ~/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
 
-# installing neovim & dependencies via apt
-apt update -y && apt install -y lua5.3 neovim python3-pip
+# installing neovim dependencies via apt
+apt update -y && apt install -y lua5.3 python3-pip
+
+# download nvim appimage
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+chmod u+x nvim.appimage && ./nvim.appimage
 
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
