305bd28b61a135b9d67c6ea5059b76c2b3be2a98
Krueger, John <john.krueger@nrel.gov>
2024-04-03 12:21:52 -0400

    no prompts

--- install.sh
+++ install.sh
@@ -8,12 +8,8 @@ ln -s init.vim ~/.config/nvim/init.vim
 ln -s neo.vim ~/.config/nvim/neo.vim
 ln -s coc-settings.json ~/.config/nvim/coc-settings.json
 
-# This will bootstrap neovim by
-# * installing neovim via apt
-# * installing vim-plug
-# * installing plugins
-# Install neovim
-apt update && apt install neovim
+# installing neovim via apt
+apt update -y && apt install -y neovim
 
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
